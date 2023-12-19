8e83315fae1474961909d0a2a651fa8fb36b39ed
Guy <guy_zxc@hotmail.com>
2023-12-19 16:01:45 +0700

    So hard

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,33 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+
+
+
+int main(){
+    const int X=100;
+    int count = 0;
+    double sum = 0,mean = 0,std = 0,STD = 0,x[X];
+    string textline;
+    ifstream source;
+    source.open("C:\\Users\\MSI_GF63\\Desktop\\Compro\\Lab10\\score.txt");
+    while(getline(source,textline)){
+        x[count]=stod(textline);
+        sum += stod(textline);
+        count++;
+    }
+    mean=sum/count;
+    for(int i=0;i<count;i++){
+       std += pow(x[i]-mean,2);
+    }
+    STD = sqrt(std/count);
+    cout << "Number of data = "<< count << endl;
+    cout << setprecision(3);
+    cout << "Mean = "<< mean << endl;
+    cout << "Standard deviation = " << STD;
+    source.close();
+    return 0;
+}
